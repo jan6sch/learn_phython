deb9b534038b5f8fdd4397b2ccddfdf57a206601
JanoschHammer <janosch.hammer06gmail.com>
2025-09-15 08:50:39 +0200

    first calculations

---
+++ 004.calculating.py
@@ -0,0 +1,7 @@
+length = float(input("How long is side A the rectangle?: "))
+width = float(input("How long is side B the rectangle?: "))
+
+area = length * width
+
+
+print(f"The rectange with a side A that is {length} long, and a side B that is {width} long, the volume is {area}")
