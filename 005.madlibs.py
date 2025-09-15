b7b5385cb094ec1f0693521463efa6f5197615f2
JanoschHammer <janosch.hammer06gmail.com>
2025-09-15 08:51:29 +0200

    easy version of madlibs

---
+++ 005.madlibs.py
@@ -0,0 +1,12 @@
+adjective1 = input("Enter an adjective: ")
+noun1 = input("Enter a noun: ")
+adjective2 = input("Enter an adjective: ")
+verb1 = input("Enter a verb ending with ing: ")
+adjective3 = input("Enter an adjective: ")
+
+
+
+print(f"Toady I went to a {adjective1} zoo")
+print(f"In an exhibit I saw a {noun1}")
+print(f"{noun1} was {adjective2} and {verb1}")
+print(f"I was {adjective3}")
