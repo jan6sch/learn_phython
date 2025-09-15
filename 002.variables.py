931bab5f9dfac5a1e4fa2bd6d027818d0204df6b
JanoschHammer <janosch.hammer06gmail.com>
2025-09-15 08:48:09 +0200

    learn variables

---
+++ 002.variables.py
@@ -0,0 +1,38 @@
+# Strings
+first_name = "Janosch"
+food = "pizza"
+
+print(f"Hello {first_name}. You like {food}.")
+
+# Integer
+age = 19
+
+print(f"You are {age} years old")
+
+# Float
+distance = 5.5
+
+print(f"You ran {distance}km")
+
+# Boolean
+
+is_student = False
+for_sale = False
+
+print(f"Are you a student? {is_student}")
+
+
+# if/else statements
+
+if for_sale:
+  print("The product is for sale")
+else:
+  print("The product isn't for sale")
+
+
+# Typecasting
+
+print(type(first_name))
+
+distance = int(distance)
+print(distance)
