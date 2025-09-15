4a63fff545bc8cde3300c6a5de9516e69e7aceff
JanoschHammer <janosch.hammer06gmail.com>
2025-09-15 08:49:50 +0200

    learn input

---
+++ 003.input.py
@@ -0,0 +1,9 @@
+name = input("What is your name?: ")
+age = int(input(f"How old are you {name}?: "))
+
+
+age = age + 1
+
+print(f"Hello {name}")
+print("Happy Birthday")
+print(f"You are {age} years old")
